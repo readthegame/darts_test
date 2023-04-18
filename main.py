d2a253fc22a919e9d2760a343fc21bf7e729b8ed
readthegame <126955479+readthegame@users.noreply.github.com>
2023-04-18 09:49:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def handler(event=None, context=None):
                               options=options)
     driver.get("https://tv.dartconnect.com/events/pdc")
     source = driver.page_source
-    events = driver.find_elements_by_css_selector('div.even\:bg-spindle-100')
+    events = driver.find_elements_by_class_name('div.even\:bg-spindle-100')
 
     driver.close();
     driver.quit();
